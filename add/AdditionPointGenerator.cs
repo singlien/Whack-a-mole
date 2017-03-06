36c9608155f71b8a2d7bf328cbee3b404f7dd808
singlien <singlien@gmail.com>
2017-03-06 11:29:47 +0800

    新增地鼠出現機率操控
    可以手動操控地鼠的出現機率

--- add/AdditionPointGenerator.cs
+++ add/AdditionPointGenerator.cs
@@ -5,12 +5,17 @@ public class AdditionPointGenerator : PointGenerator  {
 
 	public Vector2[] moleKind = new Vector2[3];
 
+	public float[] typeChance = new float[3];
+	private float[] sumArray = new float[3];
+
+
 	// Use this for initialization
 	void Start () {
 
 		//Check the settings
 		for (int i=0;i<moleKind.Length;i++) 
 		{
+			//通常不會執行，如果執行則需查看參數
 			if (moleKind[i].x == 0 || moleKind[i].y == 0) {
 				Debug.LogWarning ("Using default number area, Please check Settings in AdditionPointGenerator");
 				// Reset to default
@@ -18,13 +23,31 @@ public class AdditionPointGenerator : PointGenerator  {
 				moleKind[i].y = 9;
 			}
 		}
+
+		// Check typeChance valid
+		float check = 0;
+		foreach(float i in typeChance){ check += i; }
+		if (check != 1) {
+			Debug.LogWarning ("Generator chance settings invalid, using default chance");
+			typeChance [0] = .5f;
+			typeChance [1] = .3f;
+			typeChance [2] = .2f;
+		}
+
+		// Sum the chance and save them into an array
+		for (int i = 0; i < typeChance.Length; i++) {
+			for (int j = 0; j <= i; j++) {
+				sumArray [i] += typeChance [j];
+			}
+		}
+
 	}
 
 	public override Vector2 numberGenerator(){
 		Vector2 pointAndSeq;
 
 		//控制生成種類
-		pointAndSeq.y = Random.Range (0, moleKind.Length);
+		pointAndSeq.y = typeChooser();
 
 		float min, max;
 		min = moleKind [(int)pointAndSeq.y].x;
@@ -39,5 +62,19 @@ public class AdditionPointGenerator : PointGenerator  {
 		return pointAndSeq;
 	}
 
+	private int typeChooser(){
+		float range = Random.value;
+//		print (range);
 
+		if (0f <= range && range <= sumArray [0]) {//0~.5
+			return 0;
+		} else if (sumArray [0] < range && range <= sumArray [1]) {//.51~..7
+			return 1;	
+		} else if (sumArray [1] < range && range <= 1f) {//.7~1
+			return 2;
+		} else {
+			Debug.LogWarning ("Invalid type choosed");
+			return 0;
+		}
+	}
 }
--- div/DivisionPointGenerator.cs
+++ div/DivisionPointGenerator.cs
@@ -8,6 +8,9 @@ public class DivisionPointGenerator : PointGenerator
 	// 這邊的變數要和moleScript的陣列大小設一樣，若設定不一樣，遊戲會運作錯誤，且unity不會提出警告
 	public int moleKinds;
 
+	public float[] typeChance = new float[6];
+	private float[] sumArray = new float[6];
+
 	//宣告質數陣列
 	private int[] prime = new int[6];
 
@@ -22,13 +25,30 @@ public class DivisionPointGenerator : PointGenerator
 		prime [3] = 7;
 		prime [4] = 11;
 		prime [5] = 13;
+
+		// Check typeChance valid
+		float check = 0;
+		foreach(float i in typeChance){ check += i; }
+		if (check != 1) {
+			Debug.LogWarning ("Generator chance settings invalid, using default chance");
+			typeChance [0] = .5f;
+			typeChance [1] = .3f;
+			typeChance [2] = .2f;
+		}
+
+		// Sum the chance and save them into an array
+		for (int i = 0; i < typeChance.Length; i++) {
+			for (int j = 0; j <= i; j++) {
+				sumArray [i] += typeChance [j];
+			}
+		}
 	}
 
 	public override Vector2 numberGenerator(){
 		
 		Vector2 pointAndSeq;
 
-		pointAndSeq.y = Random.Range (0, moleKinds);		//sequence
+		pointAndSeq.y = typeChooser ();						//sequence
 		pointAndSeq.x = (int)prime [(int)pointAndSeq.y];	//point
 
 		//儲存生成數字
@@ -36,5 +56,26 @@ public class DivisionPointGenerator : PointGenerator
 
 		return pointAndSeq;
 	}
-		
+
+	private int typeChooser(){
+		float range = Random.value;
+//		print (range);
+
+		if (0f <= range && range <= sumArray [0]) {//0~.2
+			return 0;
+		} else if (sumArray [0] < range && range <= sumArray [1]) {//.21~.4
+			return 1;	
+		} else if (sumArray [1] < range && range <= sumArray [2]) {//.41~6
+			return 2;
+		} else if (sumArray [2] < range && range <= sumArray [3]) {//.61~.8
+			return 3;
+		} else if (sumArray [3] < range && range <= sumArray [4]) {//.81~.9
+			return 4;
+		} else if (sumArray [4] < range && range <= sumArray [5]) {//.91~1
+			return 5;
+		} else {
+			Debug.LogWarning ("Invalid type choosed");
+			return 0;
+		}
+	}
 }
